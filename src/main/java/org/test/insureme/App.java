2ef5f44fa780be8601468f382826e949e717f687
SriRaksha-code <131985128+SriRaksha-code@users.noreply.github.com>
2023-11-23 12:17:35 +0530

    chng

--- src/main/java/org/test/insureme/App.java
+++ src/main/java/org/test/insureme/App.java
@@ -25,7 +25,7 @@ public class App
         WebDriverManager.chromedriver().setup();
       
         //System.setProperty("webdriver.chrome.driver","C:/Users/SRI RAKSHA/Downloads/chromedriver-win64/chromedriver.exe");
-        //System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
         ChromeOptions chromeOptions = new ChromeOptions();
        chromeOptions.addArguments("--headless");
         System.out.println("Driver opening up the url in browser");
